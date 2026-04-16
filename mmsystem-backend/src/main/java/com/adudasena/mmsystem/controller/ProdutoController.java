09a5116eeb0cb855e0b56284a3f140a4892c64a8
Duda Sena <duda.sruiz@gmail.com>
2026-04-16 01:54:27 -0300

    feat(produto): expor endpoints REST para listagem e cadastro, controller

---
+++ mmsystem-backend/src/main/java/com/adudasena/mmsystem/controller/ProdutoController.java
@@ -0,0 +1,26 @@
+package com.adudasena.mmsystem.controller;
+
+import com.adudasena.mmsystem.model.Produto;
+import com.adudasena.mmsystem.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "*")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping
+    public List<Produto> listar() {
+        return service.listarTodos();
+    }
+
+    @PostMapping
+    public Produto cadastrar (@RequestBody Produto produto) {
+        return service.salvar(produto);
+    }
+}
